0223e9c4cc96eca1ea0723070f9c194a69559d19
czvy <congkvzcy@gmail.com>
2021-01-26 23:01:16 +0900

    Done

---
+++ Baekjoon Online Judge/P_2675.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class P_2675 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		for(int i =0;i<N;i++) {
+			int num = sc.nextInt();
+			String str = sc.next();
+			
+			for(int j=0;j<str.length();j++) {
+				for(int k=0;k<num;k++) {
+					System.out.print(str.charAt(j));
+				}
+			}
+			System.out.println();
+		}
+	}
+}
