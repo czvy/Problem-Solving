4621e2e18790b938277bed654b0d9144afe9a77b
czvy <congkvzcy@gmail.com>
2021-01-30 22:49:29 +0900

    Done

---
+++ Baekjoon Online Judge/P_1712.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class P_1712 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		
+		if (C <= B)
+			System.out.println(-1);
+
+		else
+			System.out.println(A / (C - B) + 1);
+	}
+}
