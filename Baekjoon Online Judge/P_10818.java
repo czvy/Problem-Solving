ae7e655da5b9acacb297f77ae9cf3eaf508ff48e
czvy <congkvzcy@gmail.com>
2021-01-31 23:22:16 +0900

    Done

--- Baekjoon Online Judge/P_10818.java
+++ Baekjoon Online Judge/P_10818.java
@@ -4,19 +4,20 @@ public class P_10818 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
-		int min=0,max=0;
-		
-		for(int i=0;i<num;i++) {
+		int min = 0, max = 0;
+
+		for (int i = 0; i < num; i++) {
 			int N = sc.nextInt();
-			if(i==0) {
-				min=N;
-				max=N;
-			}
-			else {
-				if(N<min) min=N;
-				else if(N>max) max=N;
+			if (i == 0) {
+				min = N;
+				max = N;
+			} else {
+				if (N < min)
+					min = N;
+				else if (N > max)
+					max = N;
 			}
 		}
-		System.out.println(min+" "+max);
+		System.out.println(min + " " + max);
 	}
 }
