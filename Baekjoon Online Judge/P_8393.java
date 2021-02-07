d7532a8ae71fbd88dcf18ac8398674a5e403e468
czvy <congkvzcy@gmail.com>
2021-02-07 23:15:29 +0900

    Done

--- Baekjoon Online Judge/P_8393.java
+++ Baekjoon Online Judge/P_8393.java
@@ -4,13 +4,13 @@ public class P_8393 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
-		int result=0;
-		
-		while(num>0) {
-			result+=num;
+		int result = 0;
+
+		while (num > 0) {
+			result += num;
 			num--;
 		}
-		
+
 		System.out.println(result);
 	}
 }
